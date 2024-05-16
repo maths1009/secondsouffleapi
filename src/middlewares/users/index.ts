47f874cf5f14e1b3b3c1ea166bea13907b97d630
Mathis <lb.brangeon@gmail.com>
2024-05-16 11:31:08 +0200

    📝Add documention for checkUserId middleware

--- src/middlewares/users/index.ts
+++ src/middlewares/users/index.ts
@@ -2,6 +2,15 @@ import { checkToken } from '@utils/auth'
 
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 
+/**
+ * Middleware to check if the user ID in the request parameters matches the user ID in the decoded token.
+ * If the IDs do not match, it returns a 401 Unauthorized response.
+ * Otherwise, it calls the next middleware in the chain.
+ *
+ * @param req - The Express request object.
+ * @param res - The Express response object.
+ * @param next - The next middleware function.
+ */
 export const checkUserId: RequestHandler = (
   req: Request,
   res: Response,
